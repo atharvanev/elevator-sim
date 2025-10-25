b0e21c255b39679cbaced6cbcfbdb487f073942a
Atharva <147217989+atharvanev@users.noreply.github.com>
2025-10-25 12:31:22 -0700

    init

---
+++ src/main/java/edu/ucr/cs/cs167/aneva018/App.java
@@ -0,0 +1,13 @@
+package edu.ucr.cs.cs167.aneva018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
